8416c221f6a84e30d8c4c2ed6acc828ee189db66
Chandrasekhar Kottaluru <ckottalu@oradev.oraclecorp.com>
2017-05-16 17:57:50 -0700

    chaincode

--- chaincode/project_chaincode.go
+++ chaincode/project_chaincode.go
@@ -6,9 +6,9 @@ Chaincode created for Oracle hackathon
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
-	"encoding/json"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -23,34 +23,34 @@ var projectsIndexStr = "GE::ABCConsulting"
 
 // user entered time entry
 
-type TimeEntry struct{
-	ProjectName string `json:"projectname"`
-	TaskName string `json:"taskname"`
-	User string `json:"user"`
+type TimeEntry struct {
+	ProjectName     string `json:"projectname"`
+	TaskName        string `json:"taskname"`
+	User            string `json:"user"`
 	QuantityInHours string `json:"quantityhours"`
-  TotalAmount string `json:"totalamount"`
+	TotalAmount     string `json:"totalamount"`
 }
 
 // stored as P1::User1
-type AllProjectTimeEntry struct{
+type AllProjectTimeEntry struct {
 	ProjectTimeEntry []TimeEntry `json:"project_timeentry"`
 }
 
-type ProjectMilestone struct{
-	ProjectName string `json:"projectname"`
+type ProjectMilestone struct {
+	ProjectName   string `json:"projectname"`
 	MilestoneName string `json:"milestonename"`
-	User string `json:"user"`
-  Amount string `json:"amount"`
+	User          string `json:"user"`
+	Amount        string `json:"amount"`
 }
 
 // list of project milestones , as  example P1 --> M1 1000
-type AllProjectMilestones struct{
+type AllProjectMilestones struct {
 	ProjectMileStones []ProjectMilestone `json:"project_milestones"`
 }
 
-type UserRate struct{
+type UserRate struct {
 	User string `json:"user"`
-  Rate string `json:"rate"`
+	Rate string `json:"rate"`
 }
 
 // ============================================================================================================================
@@ -69,18 +69,18 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-err := stub.PutState("test",[]byte(args[0]))
-if err != nil {
-	 return nil,err
-}
+	err := stub.PutState("test", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
 
-_,err = t.initializeData(stub,args)
+	_, err = t.initializeData(stub, args)
 
-if err != nil {
-	 return nil,err
-}
+	if err != nil {
+		return nil, err
+	}
 
-return nil, nil
+	return nil, nil
 }
 
 // Invoke is our entry point to invoke a chaincode function
@@ -88,11 +88,11 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
-	if function == "init" {													//initialize the chaincode state, used as reset
+	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
 	}
 
-	fmt.Println("invoke did not find func: " + function)					//error
+	fmt.Println("invoke did not find func: " + function) //error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
@@ -102,14 +102,14 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
-	}else if function == "read" {
-		return t.read(stub,args)
+	if function == "dummy_query" { //read a variable
+		fmt.Println("hi there " + function) //error
+		return nil, nil
+	} else if function == "read" {
+		return t.read(stub, args)
 	}
 
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function) //error
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
@@ -118,53 +118,52 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	var key, jsonResp string
 	var err error
 
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-    }
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
 
-    key = args[0]
-    valAsbytes, err := stub.GetState(key)
-    if err != nil {
-        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-        return nil, errors.New(jsonResp)
-    }
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
 
-    return valAsbytes, nil
+	return valAsbytes, nil
 }
-
 
 //Initilizing project Data
 
-func (t *SimpleChaincode) initializeData(stub shim.ChaincodeStubInterface,args []string) ([]byte, error){
-  //Initilizing the sample projects
-  consultingProjects := []string {"Proj1","Proj2","Proj3"}
+func (t *SimpleChaincode) initializeData(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	//Initilizing the sample projects
+	consultingProjects := []string{"Proj1", "Proj2", "Proj3"}
 
-  jsonAsBytes, _ := json.Marshal(consultingProjects)
+	jsonAsBytes, _ := json.Marshal(consultingProjects)
 	err := stub.PutState(projectsIndexStr, jsonAsBytes)
 	if err != nil {
 		return nil, err
 	}
 
- //Initilizing the Project user rates p1 -->[ {u1 100}, {u2 200 }]
- var projectUserRates []UserRate
- userrate := UserRate{}
- userrate.User = "Chandra"
- userrate.Rate = "90"
- projectUserRates = append(projectUserRates,userrate)
+	//Initilizing the Project user rates p1 -->[ {u1 100}, {u2 200 }]
+	var projectUserRates []UserRate
+	userrate := UserRate{}
+	userrate.User = "Chandra"
+	userrate.Rate = "90"
+	projectUserRates = append(projectUserRates, userrate)
 
- userrate.User = "Sudheer"
- userrate.Rate = "100"
- projectUserRates = append(projectUserRates,userrate)
+	userrate.User = "Sudheer"
+	userrate.Rate = "100"
+	projectUserRates = append(projectUserRates, userrate)
 
- userrate.User = "Sanjay"
- userrate.Rate = "80"
- projectUserRates = append(projectUserRates,userrate)
+	userrate.User = "Sanjay"
+	userrate.Rate = "80"
+	projectUserRates = append(projectUserRates, userrate)
 
- jsonAsBytes, _ = json.Marshal(projectUserRates)
- err = stub.PutState("Proj1", jsonAsBytes)
- if err != nil {
-   return nil, err
- }
+	jsonAsBytes, _ = json.Marshal(projectUserRates)
+	err = stub.PutState("Proj1", jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
 
-return nil,nil
+	return nil, nil
 }
