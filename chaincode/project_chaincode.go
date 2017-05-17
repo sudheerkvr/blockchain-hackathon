7b19261e5228aa89ddab731a8769cef890479a9c
Sudheer Kola <sudheerkvr@gmail.com>
2017-05-17 01:33:04 -0700

    Added debug messages

--- chaincode/project_chaincode.go
+++ chaincode/project_chaincode.go
@@ -259,7 +259,7 @@ func (t *SimpleChaincode) create_time_entry(stub shim.ChaincodeStubInterface, ar
 	}*/
 	
 	//build the marble json string manually
-	timeEntryAsBytes := []byte(`{"projectname": "` + projectname + `", "taskname": "` + taskname + `", "personname": "` + personname + `", "quantityinhours": ` + strconv.Itoa(quantity) + `", "expensetype": "` + expensetype + `", "totalamount": ` + strconv.Itoa(totalAmount) + `"}`)
+	timeEntryAsBytes := []byte(`{"projectname": "` + projectname + `", "taskname": "` + taskname + `", "personname": "` + personname + `", "quantityinhours": "` + strconv.Itoa(quantity) + `", "expensetype": "` + expensetype + `", "totalamount": "` + strconv.Itoa(totalAmount) + `"}`)
 
 	fmt.Println("timeEntryAsBytes : ", timeEntryAsBytes);
 	fmt.Println("timeEntryAsBytes as a string : ", string(timeEntryAsBytes));
